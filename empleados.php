0d65b123a2b54a7b04b89a530ac34fd6585d1874
juanmautem <manuel-fernandez@utem.edu.mx>
2022-06-06 15:31:02 -0500

    Agregando vista empleados.php

---
+++ empleados.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Vista Empleados</title>
+</head>
+<body>
+	<?php print_r($_SESSION); ?>
+
+</body>
+</html>
