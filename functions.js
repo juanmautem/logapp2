6c845340b510128a679dcdc5b430c421876c6bd0
juanmautem <manuel-fernandez@utem.edu.mx>
2022-06-06 16:10:30 -0500

    Separación de archivo JS

---
+++ functions.js
@@ -0,0 +1,87 @@
+
+function allTypes(types){
+        let typeList = document.getElementById("list1");
+        var cad = "<option value = '0';>Seleccione Opción</option>";
+		for (var row of types){
+			cad += "<option title='" + row.Description + "'  value = '" + row.ID + "';>" + row.Type + "</option>";
+		}
+		typeList.innerHTML = cad;	
+	}
+
+    function allUsers(allUsers){
+        let usersList = document.getElementById("tbAllUsers");
+        var cad = "";
+        for (var row of allUsers){
+            cad += "<tr>";
+            cad += "<td>" + row.UserID + "</td>";
+            cad += "<td>" + row.txtNombre + " " + row.txtApellidos + "</td>";
+            cad += "<td>" + row.txtRFC + "</td>";
+            cad += "<td>" + row.txtNombreCorto + "</td>";
+            cad += "<td>";
+                if(row.bActivo == 1)
+                    cad += "<button value='"+row.UserID +"' class='btn btn-success btn-sm ' onclick = 'change(this.value);' > <i class='fas fa-thumbs-up'></i></button>";
+                if(row.bActivo == 0)
+                    cad += "<button value='"+row.UserID +"' class='btn btn-danger btn-sm ' onclick = 'change(this.value);' > <i class='fas fa-thumbs-down'></i></button>";
+            cad += "</td>";
+            cad += "<td> </td>";
+            cad+= "</tr>"
+        }
+        usersList.innerHTML = cad;
+    }
+	function change(id){
+	var usrData = {
+            "userID" : id,
+            "typeOp" : 1
+        };
+		$.ajax({
+            method: "GET",
+            url: "modUser.php",
+            data: usrData,
+            success: function(data){
+            	data = $.trim(data);
+            	if(data == 'true')
+            		location.reload();
+
+            }
+        });
+	}
+
+	function login(){
+        var usr = document.getElementById("usr");
+        let psw = document.getElementById("psw");
+        var usrData = {
+            "user" : usr.value,
+            "pass" : psw.value,
+            "typeOp" : 1
+        };
+		
+        $.ajax({
+            method: "GET",
+            url: "modLogin.php",
+            data: usrData,
+            success: function(data){
+            	data = $.trim(data);
+            	switch(data){
+            		case '1':
+            			alert("BIENVENIDO SUPERADMIN");
+            			window.location.href = "./superAdmin.php?access=true";
+            			break;
+            		case '2':
+            			alert("BIENVENIDO ADMIN");
+            			window.location.href = "./admin.php";
+            			break;
+        			case '3':
+	        			alert("BIENVENIDO EMPLEADO");
+            			window.location.href = "./empleados.php";
+	        			break;
+        			case '4':
+	        			alert("BIENVENIDO CLIENTE");
+            			window.location.href = "./clientes.php";
+	        			break;
+        			default:
+	        			alert("USUARIO NO ENCONTRADO");
+	        			break;
+            	}
+            }
+        });
+	}
--- index_0.php
+++ index_0.php
@@ -34,8 +34,6 @@ session_destroy();
     <!-- Librería AJAX -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-    <script src="functions.js" language="Javascript"></script> 
-
 </head>
 <style type="text/css">
 	body{
@@ -149,14 +147,101 @@ session_destroy();
         </section>
 	<footer class="footer"></footer>
 </body>
-<script type="text/javascript">
-window.onload = function() {
-    
-    var types = <?php print_r(types());?>;
-    var users = <?php print_r(allUsers());?>;
-    allTypes(types);
-    allUsers(users);
 
-}
+<script type="text/javascript">
+
+ 	window.onload = function() {
+ 		allTypes();
+ 		allUsers();
+
+ 	}
+	function allTypes(){
+        let typeList = document.getElementById("list1");
+        var cad = "<option value = '0';>Seleccione Opción</option>";
+		var types = <?php print_r(types());?>;
+		for (var row of types){
+			cad += "<option title='" + row.Description + "'  value = '" + row.ID + "';>" + row.Type + "</option>";
+		}
+		typeList.innerHTML = cad;	
+	}
+
+    function allUsers(){
+        let usersList = document.getElementById("tbAllUsers");
+        var cad = "";
+        var allUsers = <?php print_r(allUsers());?>;
+        for (var row of allUsers){
+            cad += "<tr>";
+            cad += "<td>" + row.UserID + "</td>";
+            cad += "<td>" + row.txtNombre + " " + row.txtApellidos + "</td>";
+            cad += "<td>" + row.txtRFC + "</td>";
+            cad += "<td>" + row.txtNombreCorto + "</td>";
+            cad += "<td>";
+                if(row.bActivo == 1)
+                    cad += "<button value='"+row.UserID +"' class='btn btn-success btn-sm ' onclick = 'change(this.value);' > <i class='fas fa-thumbs-up'></i></button>";
+                if(row.bActivo == 0)
+                    cad += "<button value='"+row.UserID +"' class='btn btn-danger btn-sm ' onclick = 'change(this.value);' > <i class='fas fa-thumbs-down'></i></button>";
+            cad += "</td>";
+            cad += "<td> </td>";
+            cad+= "</tr>"
+        }
+        usersList.innerHTML = cad;
+    }
+	function change(id){
+	var usrData = {
+            "userID" : id,
+            "typeOp" : 1
+        };
+		$.ajax({
+            method: "GET",
+            url: "modUser.php",
+            data: usrData,
+            success: function(data){
+            	data = $.trim(data);
+            	if(data == 'true')
+            		location.reload();
+
+            }
+        });
+	}
+
+	function login(){
+        var usr = document.getElementById("usr");
+        let psw = document.getElementById("psw");
+        var usrData = {
+            "user" : usr.value,
+            "pass" : psw.value,
+            "typeOp" : 1
+        };
+		
+        $.ajax({
+            method: "GET",
+            url: "modLogin.php",
+            data: usrData,
+            success: function(data){
+            	data = $.trim(data);
+            	switch(data){
+            		case '1':
+            			alert("BIENVENIDO SUPERADMIN");
+            			window.location.href = "./superAdmin.php?access=true";
+            			break;
+            		case '2':
+            			alert("BIENVENIDO ADMIN");
+            			window.location.href = "./admin.php";
+            			break;
+        			case '3':
+	        			alert("BIENVENIDO EMPLEADO");
+            			window.location.href = "./empleados.php";
+	        			break;
+        			case '4':
+	        			alert("BIENVENIDO CLIENTE");
+            			window.location.href = "./clientes.php";
+	        			break;
+        			default:
+	        			alert("USUARIO NO ENCONTRADO");
+	        			break;
+            	}
+            }
+        });
+	}
 </script>
 </html>
